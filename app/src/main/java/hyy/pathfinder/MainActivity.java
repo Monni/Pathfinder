c07ef2e27711c07f3d714a248fbee9780507e792
Miika Avela <h4211@student.jamk.fi>
2016-09-13 18:35:19 +0300

    JSON parsing now works..
    Got JSON parsing working, also saves data into a two-dimensional array

--- app/src/main/java/hyy/pathfinder/MainActivity.java
+++ app/src/main/java/hyy/pathfinder/MainActivity.java
@@ -3,41 +3,37 @@ package hyy.pathfinder;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-    private JSONArray stations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FetchDataTask task = new FetchDataTask();
-        //task.execute("http://ptm.fi/android/highscore.json");
-        task.execute("http://rata.digitraffic.fi/api/v1/metadata/stations.json");
+        task.execute("http://rata.digitraffic.fi/api/v1/metadata/stations.json"); // Get current list of train stations
     }
 
-    class FetchDataTask extends AsyncTask<String, Void, JSONObject> {
+    class FetchDataTask extends AsyncTask<String, Void, JSONArray> {
         @Override
-        protected JSONObject doInBackground(String... urls) {
+        protected JSONArray doInBackground(String... urls) {
             HttpURLConnection urlConnection = null;
-            JSONObject json = null;
+            JSONArray json = null;
             try {
                 URL url = new URL(urls[0]);
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -48,40 +44,39 @@ public class MainActivity extends AppCompatActivity {
                     stringBuilder.append(line).append("\n");
                 }
                 bufferedReader.close();
-                json = new JSONObject(stringBuilder.toString());
+                json = new JSONArray(stringBuilder.toString());
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
-                e.printStackTrace();
+                    e.printStackTrace();
             } finally {
                 if (urlConnection != null) urlConnection.disconnect();
             }
             return json;
         }
 
-        protected void onPostExecute(JSONObject json) {
+        protected void onPostExecute(JSONArray json) {
             StringBuffer text = new StringBuffer("");
+            List<String[]> stationList = new ArrayList<String[]>();
             try {
-    //            stations = json.getJSONArray("highscores");
-               JSONArray test = new JSONArray(json);
-
-                //
-                //for (int i=0;i < test.length();i++) {
-                  //  JSONObject hs = test.getJSONObject(i);
-                   // text.append(hs.getString("stationName")+ "\n");
-                //}
-
-               // for (int i=0;i < stations.length();i++) {
-                 //   JSONObject hs = stations.getJSONObject(i);
-                   // text.append(hs.getString("stationName") +"\n");
-                //}
+                  for (int i=0; i < json.length(); i++) {
+                    JSONObject station = json.getJSONObject(i);
+                      if (station.getString("passengerTraffic") == "true") {
+                          // Add data to a two-dimensional array of passenger stations in Finland, including longitude and latitude
+                          stationList.add(new String[] {station.getString("stationName"), station.getString("longitude"), station.getString("latitude")});
+                          text.append(station.getString("stationName") + "\n"); // Just for debug! not needed
+                      }
+                }
             } catch (JSONException e) {
                 Log.e("JSON", "Error getting data.");
             }
 
+            String text2 = stationList.get(0)[0].toString(); // Just for debugging! shows wanted stations on screen
             TextView textView = (TextView) findViewById(R.id.textView);
-            textView.setText(text);
+            textView.setText(text2);
         }
     }
+
+
 
 }
